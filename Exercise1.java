abfdf4c20b000b82c279cde9c5d144b55ff00e54
Lanhh Nhoo <xuanlanhbk0104@gmail.com>
2021-02-23 17:05:52 +0700

    Add Exercise1

---
+++ Exercise1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String object;
+        System.out.println("What do you like to enter:vvv");
+        object = scanner.nextLine();
+        System.out.println("Oki. Your string entered : " + object);
+    }
+}
